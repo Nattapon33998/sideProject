5e22171dd3e2eab6fef19792f44532e6dbaf1155
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-09-11 00:47:22 +0700

    init scrollBar

---
+++ 05_ScrollBar/scroll-bar/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Scrollbar from "./components/ScrollBar";
+
+function App() {
+  return (
+    <div>
+      <Scrollbar />
+    </div>
+  );
+}
+
+export default App;
