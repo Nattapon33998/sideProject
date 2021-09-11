fcdd8a1b518b18c9f44385c85b529d1b2aade9a0
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-09-12 00:26:16 +0700

    product card

---
+++ 06_ProductCard/product-card/src/components/ProductCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import shoes from "../images/Nike01.png";
+import "./ProductCard.css";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="card">
+          <div className="imgBx">
+            <img src={shoes} />
+          </div>
+          <div className="contentBx">
+            <h2>Nike Shoes</h2>
+            <div className="size">
+              <h3>Size :</h3>
+              <span>7</span>
+              <span>8</span>
+              <span>9</span>
+              <span>10</span>
+            </div>
+          </div>
+          <div className="color">
+            <h3>Color :</h3>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <a href="#">Buy Now</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
