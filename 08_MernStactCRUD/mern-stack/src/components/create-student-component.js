dff68abac68167b5970e4658c5af9f34d30306c2
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-09-15 00:29:56 +0700

    create student

--- 08_MernStactCRUD/mern-stack/src/components/create-student-component.js
+++ 08_MernStactCRUD/mern-stack/src/components/create-student-component.js
@@ -1,10 +1,31 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const CreateStudent = () => {
   return (
-    <div>
+    <div className="form-wrapper mt-5">
       <h1>Create Student</h1>
-      <h1>Create Student</h1>
+      <Form>
+        <Form.Group controlId="Name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+
+        <Form.Group controlId="Name">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+
+        <Form.Group controlId="Name">
+          <Form.Label>Roll No.</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+
+        <Button variant="success" size="lg" block="block" type="submit">
+          Create Student
+        </Button>
+      </Form>
     </div>
   );
 };
