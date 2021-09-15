fd72616440c5271694d3a05a2a5b2a11e2c94975
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-09-15 14:36:13 +0700

    router complet

--- 08_MernStactCRUD/mern-stack/backend/routes/student.route.js
+++ 08_MernStactCRUD/mern-stack/backend/routes/student.route.js
@@ -16,3 +16,59 @@ router.route("/create-student").post((req, res, next) => {
     }
   });
 });
+
+// read student
+router.route("/").get((req, res) => {
+  studentSchema.find((error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+// get single student
+router.route("/edit-student/:id").get((req, res) => {
+  studentSchema.findById(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+// update student
+router.route("/update-student/:id").put((req, res, next) => {
+  studentSchema.findByUdAndUpdated(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (error) {
+        return next(error);
+        console.log(error);
+      } else {
+        res.json(data);
+        console.log("student update success");
+      }
+    }
+  );
+});
+
+// delete student
+router.route("/delete-student/:id").delete((req, res, next) => {
+  studentSchema.findByUdAndDelete(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json({
+        msg: data,
+      });
+    }
+  });
+});
+
+module.exports = router;
