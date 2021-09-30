fa533ad5aa5a257ea10cc23d1d71fd9652537ff5
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-09-30 14:08:19 +0700

    smooth scroll

--- 09_SmoothScroll/smooth-scroll/src/App.js
+++ 09_SmoothScroll/smooth-scroll/src/App.js
@@ -4,8 +4,21 @@ import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div>
+    <div id="home">
       <Navbar />
+      <section className="home">
+        <h1>home</h1>
+      </section>
+      <section className="about" id="about">
+        <h1>about</h1>
+      </section>
+      <section className="projects" id="projects">
+        <h1>projects</h1>
+      </section>
+      <section className="contact" id="contact">
+        <h1>contact</h1>
+      </section>
+      <section className="rest"></section>
     </div>
   );
 }
--- 09_SmoothScroll/smooth-scroll/src/Navbar.js
+++ 09_SmoothScroll/smooth-scroll/src/Navbar.js
@@ -3,12 +3,22 @@ import "./Navbar.css";
 import { links } from "./data";
 
 const Navbar = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const target = e.target.getAttribute("href");
+    const location = document.querySelector(target).offsetTop;
+
+    window.scrollTo({
+      left: 0,
+      top: location - 64,
+    });
+  };
   return (
     <div className="navbar sticky">
       <div className="nav-center">
         {links.map((link) => {
           return (
-            <a href={link.url} key={link.id}>
+            <a href={link.url} key={link.id} onClick={handleClick}>
               {link.text}
             </a>
           );
