58e6143d1827f295b54ca8167e6f74a0c4e75241
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-10-02 22:26:36 +0700

    navbar

---
+++ 11_CV/cv/src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link as LinkS } from "react-scroll";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div className="navContainer">
+        <LinkS className="navLogo">O@T</LinkS>
+        <ul className="navMenu">
+          <li className="navItem">
+            <LinkS className="navLink">About</LinkS>
+          </li>
+          <li className="navItem">
+            <LinkS className="navLink">Experiance</LinkS>
+          </li>
+          <li className="navItem">
+            <LinkS className="navLink">Projects</LinkS>
+          </li>
+          <li className="navItem">
+            <LinkS className="navLink">Education</LinkS>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
