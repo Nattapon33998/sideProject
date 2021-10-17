b813eb4b093a6d6a7523d0806dcf1edbc069a27a
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-10-17 12:08:56 +0700

    edit deploy

--- 12_Web3_Py_Simple_Store/deploy.py
+++ 12_Web3_Py_Simple_Store/deploy.py
@@ -1,3 +1,20 @@
+from solcx import compile_standard
+
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+    # print(simple_storage_file)
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "setting": {
+            "outputSelections": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sorceMap"]}
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+print(compiled_sol)
