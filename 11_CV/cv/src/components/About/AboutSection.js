7a6bbba1f9f078e7303bb70258b86f10d39f1996
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-10-20 02:08:27 +0700

    about section

---
+++ 11_CV/cv/src/components/About/AboutSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <div className="aboutContainer">
+      <div className="aboutContent"></div>
+      <img />
+    </div>
+  );
+};
+
+export default AboutSection;
