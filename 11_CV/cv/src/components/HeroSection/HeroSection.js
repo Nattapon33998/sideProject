847b6f81fb1fc9f206cfe0e86f6c4a0988a19b6f
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-10-20 01:52:22 +0700

    complet hero

--- 11_CV/cv/src/components/HeroSection/HeroSection.js
+++ 11_CV/cv/src/components/HeroSection/HeroSection.js
@@ -21,8 +21,26 @@ const HeroSection = () => {
       <div className="heroContent">
         <img className="profile" src={profile} />
         <div className="textContent">
-          <h1 className="textH1">Nattapon</h1>
-          <p className="textP">babababababababababababababababababababababab</p>
+          <h1>Hey there!</h1>
+          <h1>I'm Nattapon Sukthaworn</h1>
+          <div style={{ display: "flex" }}>
+            <h2 style={{ marginTop: "0", marginBottom: "0" }}>Born to be</h2>
+            <h2
+              style={{
+                color: "#01bf71",
+                padding: "0px 7px",
+                marginTop: "0",
+                marginBottom: "0",
+              }}
+            >
+              Developer
+            </h2>
+          </div>
+          <div className="btnWrap">
+            <div className className="btnCV">
+              Download CV
+            </div>
+          </div>
         </div>
       </div>
     </div>
