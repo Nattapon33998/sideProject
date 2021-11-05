4428fdc1d195affaefa3480b0fab9146db70bc25
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-11-05 22:16:53 +0700

    add ethscan

--- 11_CV/cv/src/components/About/AboutSection.js
+++ 11_CV/cv/src/components/About/AboutSection.js
@@ -3,8 +3,9 @@ import React from "react";
 const AboutSection = () => {
   return (
     <div className="aboutContainer">
-      <div className="aboutContent"></div>
-      <img />
+      <div className="aboutContent">
+        <img />
+      </div>
     </div>
   );
 };
